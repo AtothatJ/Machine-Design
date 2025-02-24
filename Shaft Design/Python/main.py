611cb526237866c0e76ebdf9084d88ccfcb6ad63
Aj Hall <ajhall711@gmail.com>
2025-02-24 15:32:25 -0700

    Updating goodman criteria part one, to reflect the change in assignment

--- Shaft Design/Python/main.py
+++ Shaft Design/Python/main.py
@@ -1,47 +1,38 @@
 from math import pi, sqrt
 
 # Declaring given info from the problem statement
-moment = 100
-torque = 200
+moment = 400
+torque = 100
 diameter = 0.75
 
-# Declaring material properties for 1030 HR steel
-# Declaring material properties (HR 1030 Steel)
+
+# Declaring material properties for 1030 machined steel
 Sut = 68  # ksi
 Sy = 42000
 sePrime = 0.5 * Sut
-
 
 def goodman_criteria_one():
     """
     Calculates the goodman criteria for questions 1-10.
     """
-    # Calculating K values
-    a = 11
-    b = -0.650
-    ka = a * Sut ** b
-    kb = 0.879 * diameter ** -0.107
-    kc = 1
-    # Problem statement does not include a temp, so skipping Kd
-    za = 2.326
-    ke = 1 - 0.08 * za
-    Se = ka * kb * kc * ke * sePrime
-    kf = 1.5
-    # Calculating alternating bending stress
-    sigmaA = kf * (32 * moment) / (pi * diameter ** 3)
-    # Calculating alternating torsional stress
-    tauA = kf * (16 * torque) / (pi * diameter ** 3)
-
-    # Declaring mean torsional stress and bending stress
-    # Due to the loading configuration, there are no mean stresses
-    tauM = sigmaM = 0
-
-    # Calculating Von mises alternating stress
-    sigmaPrimeA = sqrt(sigmaA ** 2 + 3 * tauA ** 2)
-
-    # Calculating Von mises mean stress
-    sigmaPrimeM = sqrt(sigmaM ** 2 + 3 * tauM ** 2)
-
+    sharp = 0
+    if sharp == 1:
+        rBending = 2.7
+        rTorsion = 2.2
+    elif sharp == 0
+        rBending = 1.7
+        rTorsion = 1.5
+    Kf =
+    Kfs =
+    SePrime = 0.5*Sut
+    Ka =
+    Kb =
+    Kc =
+    Kd =
+    Ke =
+    Se = Ka * Kb * Kc * Kd * Ke * SePrime
+    SigmaPrimeA = sqrt( (32*) )
+    SigmaPrimeM = sqrt(  )
     # Calculating Goodman Criterion
     Nf = 1 / ((sigmaPrimeA / Se) + (sigmaPrimeM / Sut))
 
