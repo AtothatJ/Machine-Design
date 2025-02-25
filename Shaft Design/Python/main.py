6843740eec167992ba4785ea0978094e0da005e9
Aj Hall <ajhall711@gmail.com>
2025-02-24 19:28:10 -0700

    Commented and reviewed von mises stress

--- Shaft Design/Python/main.py
+++ Shaft Design/Python/main.py
@@ -3,14 +3,21 @@ from math import pi, sqrt
 
 def goodman_criteria_one():
     """
-    Calculates the goodman criteria for questions 1-10. Assuming machined steel
+    Calculates the goodman criteria for questions 1-10
     """
+    # Calculating sqrt(a)
     bendingRootA = 0.246 - (3.08 * 10 ** -3) * Sut + (1.51 * 10 ** -5) * Sut ** 2 - (2.67 * 10 ** -8) * Sut ** 3
     torsionalRootA = 0.190 - (2.51 * 10 ** -3) * Sut + (1.35 * 10 ** -5) * Sut ** 2 - (2.67 * 10 ** -8) * Sut ** 3
+
+    # Calculating q values
     qBending = 1 / (1 + (bendingRootA / rootR))
     qTorsional = 1 / (1 + (torsionalRootA / rootR))
+
+    # converting Kt and Kts
     Kf = 1 +  qBending * (Kt - 1)
     Kfs = 1 + qTorsional * (Kts - 1)
+
+    # Calculating K (a-e) values
     a = 2
     b = -0.217
     Ka = a * Sut ** b
@@ -18,12 +25,17 @@ def goodman_criteria_one():
     Kc = 1
     Kd = 1
     Ke = 1
+
+    #calculating Se
     Se = Ka * Kb * Kc * Kd * Ke * sePrime
+
     # Calculating stress components
     A = sqrt(4 * (Kf * moment) ** 2)
     B = sqrt(3* (Kfs * torque) ** 2)
-    # Calculating Goodman Criterion
+
+    # Calculating safety factor
     Nf = ((pi * diameter ** 3) / 16) * ((A / Se) + (B / Sut)) ** -1
+
     # Showing the answer to the user
     print('The factor of safety calculated from the Goodman criteria is : ' + str(Nf))
 
@@ -31,21 +43,9 @@ def goodman_criteria_one():
 def vonmises_stress():
     """
     Calculates the safety factor against first cycle yielding, using Von Mises.
-    Questions 11-20
+    Questions ___
     """
-    kf = 1
-
-    # Calculating bending stress
-    sigma = kf * 32 * moment / (pi * diameter ** 3)
-
-    # Calculating shear stress
-    tau = 16 * torque / (pi * diameter ** 3)
-
-    # Calculating von mises stress
-    sigmaPrime = sqrt(sigma ** 2 + 3 * tau ** 2)
-
-    # Calculating the von mises safety factor
-    safetyFactor = Sy / sigmaPrime
+    #
 
     # Showing the answer to the user
     print('The factor of safety calculated from the von Mises stress is : ' + str(safetyFactor))
@@ -53,18 +53,19 @@ def vonmises_stress():
 
 def goodman_criteria_two():
     """
-    Calculates the goodman criteria for questions 21-30.
+    Calculates the goodman criteria for questions ____
     """
     Nf = 1
     print('The factor of safety calculated from the Goodman criteria is : ' + str(Nf))
 
 
 if __name__ == "__main__":
-    # Declaring material properties for 1030 machined steel
+    # Declaring material properties for 1030 HR steel (machined)
     Sut = 68  # ksi
     Sy = 37.5
     sePrime = 0.5 * Sut
 
+    # Accepting user input, data collected from problem statement
     moment = int(input("Moment: ")) / 1000
     torque = int(input("Torque: ")) / 1000
     diameter = float(input("Diameter: "))
@@ -75,6 +76,7 @@ if __name__ == "__main__":
     print("For the first cycle yield using conservative approximation and infinite life using the goodman criteria, enter '3'")
     problemType = eval(input("Problem type: "))
 
+    # Allowing the user to select sharp or wide radius
     if radiusType == 0:
         # Sharp radius
         Kt = 2.7
@@ -86,6 +88,7 @@ if __name__ == "__main__":
         Kts = 1.5
         rootR = sqrt(diameter * 0.1)
 
+    # Allowing the user to select a problem type
     if problemType == 1:
         goodman_criteria_one()
     elif problemType == 2:
