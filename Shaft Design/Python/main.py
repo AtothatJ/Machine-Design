7a8edfe1e224ffac246dea8758bc70bc411914e4
Aj Hall <ajhall711@gmail.com>
2025-02-24 17:25:03 -0700

    Finalized part A

--- Shaft Design/Python/main.py
+++ Shaft Design/Python/main.py
@@ -1,21 +1,24 @@
 from math import pi, sqrt
 
 # Declaring given info from the problem statement
-moment = 400
-torque = 100
-diameter = 0.75
+moment = 400/1000
+torque = 200/1000
+diameter = 1.5
 
+# Kt and Kts values from
 # KT KTS values for sharp radius
 #Kt = 2.7
 #Kts = 2.2
+#rootR = sqrt(diameter * 0.02)
 
 # KT KTS values for wide radius
 Kt = 1.7
 Kts = 1.5
+rootR = sqrt(diameter * 0.1)
 
 # Declaring material properties for 1030 machined steel
-Sut = 68 * 10 ** 3  # ksi
-Sy = 42000
+Sut = 68 # ksi
+Sy = 37.5
 sePrime = 0.5 * Sut
 
 
@@ -24,13 +27,11 @@ def goodman_criteria_one():
     Calculates the goodman criteria for questions 1-10. Assuming machined steel
     """
     bendingRootA = 0.246 - (3.08 * 10 ** -3) * Sut + (1.51 * 10 ** -5) * Sut ** 2 - (2.67 * 10 ** -8) * Sut ** 3
-    torsionalRootA = 0.190 - (135 * 10 ** -3) * Sut + (1.35 * 10 ** -6) * Sut ** 2 - (2.67 * 10 ** -8) * Sut ** 3
-    rootR = sqrt(diameter/2)
+    torsionalRootA = 0.190 - (2.51 * 10 ** -3) * Sut + (1.35 * 10 ** -5) * Sut ** 2 - (2.67 * 10 ** -8) * Sut ** 3
     qBending = 1 / (1 + (bendingRootA / rootR))
     qTorsional = 1 / (1 + (torsionalRootA / rootR))
-    Kf = 1 + (Kt * qBending)
-    Kfs = 1 + (Kts * qTorsional)
-    SePrime = 0.5*Sut
+    Kf = 1 +  qBending * (Kt - 1)
+    Kfs = 1 + qTorsional * (Kts - 1)
     a = 2
     b = -0.217
     Ka = a * Sut ** b
@@ -38,11 +39,12 @@ def goodman_criteria_one():
     Kc = 1
     Kd = 1
     Ke = 1
-    Se = Ka * Kb * Kc * Kd * Ke * SePrime
-    sigmaPrimeA = sqrt(((32 * Kf * moment) ** 2 )/ (pi * diameter ** 3))
-    sigmaPrimeM = sqrt((3 * (16 * Kfs * torque) ** 2 /  (pi * diameter )**3))
+    Se = Ka * Kb * Kc * Kd * Ke * sePrime
+    # Calculating stress components
+    A = sqrt(4 * (Kf * moment) ** 2)
+    B = sqrt(3* (Kfs * torque) ** 2)
     # Calculating Goodman Criterion
-    Nf = 1 / ((sigmaPrimeA / Se) + (sigmaPrimeM / Sut))
+    Nf = ((pi * diameter ** 3) / 16) * ((A / Se) + (B / Sut)) ** -1
     # Showing the answer to the user
     print('The factor of safety calculated from the Goodman criteria is : ' + str(Nf))
 
